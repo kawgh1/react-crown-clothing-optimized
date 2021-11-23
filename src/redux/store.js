dc43ed92512f502efa08206918c0460de90573a4
kawgh1 <krseven@gmail.com>
2021-11-23 04:13:05 -0600

    updated redux components to implement sagas - cart, shop and user

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 
 import logger from "redux-logger";
 
-// Redux Thunk
-import thunk from "redux-thunk";
+// Redux Thunk has been replaced with Redux Sagas
+// import thunk from "redux-thunk";
 
 import rootReducer from "./root-reducer";
 
@@ -11,15 +11,24 @@ import rootReducer from "./root-reducer";
 // Allows redux to persist the Store depending on certain config options set
 import { persistStore } from "redux-persist";
 
+// REDUX SAGAS
+import createSagaMiddleware from "@redux-saga/core";
+
+const sagaMiddleware = createSagaMiddleware();
 // The Middleware that the Store is expecting from Redux is an array []
 
-const middlewares = [thunk];
+// Redux Thunk has been replaced with Redux Sagas
+// const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 // only run logger (in browser console) in development mode, not production
 if (process.env.NODE_ENV === "development") {
     middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// redux-saga
+// sagaMiddleware.run();
 
 //redux-persist
 export const persistor = persistStore(store);
