005197bdeaa3699f8fb6e9339929076d56cc3cde
kawgh1 <krseven@gmail.com>
2021-11-26 07:18:06 -0600

    setting up backend payment method

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publicKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
 
     const onToken = (token) => {
-        console.log(token);
-        alert("Payment Successful");
+        // console.log(token);
+        // alert("Payment Successful");
+        // send total and token to backend for stripe processing of payment
+        axios({
+            url: "payment",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token,
+            },
+        })
+            .then((response) => {
+                alert("Payment successful");
+            })
+            .catch((error) => {
+                console.log("Payment error: ", JSON.parse(error));
+                alert(
+                    "Payment Unsuccessful: We encountered an error when processing your payment. Please use the provided test credit card. "
+                );
+            });
     };
 
     return (
