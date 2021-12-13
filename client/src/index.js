64b4f215b5acafe70ccfd521cbff44e82d3d49e4
kawgh1 <krseven@gmail.com>
2021-12-13 15:07:43 -0600

    retrying service worker for heroku deploy

--- client/src/index.js
+++ client/src/index.js
@@ -19,20 +19,20 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 
 // PWA
-import registerServiceWorker from "react-service-worker";
-
-const appSW = registerServiceWorker();
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <PersistGate persistor={persistor}>
-                <App appServiceWorker={appSW} />
+                <App />
             </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById("root")
 );
+
+serviceWorker.register();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
